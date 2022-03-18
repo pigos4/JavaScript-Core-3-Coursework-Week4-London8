4324200916785cfa718f35c6640d6b02a947dbed
Daniel <pigos487@gmail.com>
2022-03-18 10:32:30 +0000

    a

--- C-TDD/02-password-verifier/password-verifier.js
+++ C-TDD/02-password-verifier/password-verifier.js
@@ -2,6 +2,5 @@ const verify = (pass) => {
 if(!pass||pass===null)return "Password rejected";
 if (pass.length > 7 && pass.match(/[0-9]/g)) return "Password accepted";
 };
-
-
+//h
 module.exports = verify;
